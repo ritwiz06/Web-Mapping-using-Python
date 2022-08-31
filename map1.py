b8e7ef4f27c2ac2739422d9bdb2499a9e937423c
ritwiz06 <ritik.agrawal06@gmail.com>
2022-09-01 00:34:25 +0530

    Completed by adding population and layer control

--- map1.py
+++ map1.py
@@ -9,33 +9,38 @@ elev = list(vol_data["Elev"])
 
 
 html = """
-Volcano name:<br>
+Volcano name:
 <a href="https://www.google.com/search?q=Volcano%%20%%22%s%%22"
 target="_blank">%s</a><br>
 Height: %s m
 """
 
 def color_prod(ev):
-    if ev<-3000:
-        return 'beige'
-    elif -3000<=ev<0:
+    if ev<-2000:
+        return 'yellow'
+    elif -2000<=ev<1000:
         return 'orange'
-    elif 0<=ev<2000:
-        return 'lightred'
-    elif 2000<=ev<4000:
+    elif 1000<=ev<3500:
         return 'red'
     else:
-        return 'darkred'
+        return 'maroon'
 
 
 
 map = folium.Map(location = [38.5,-99], zoom_start=3, tiles="Stamen Terrain")
-fg= folium.FeatureGroup(name="My Map")
-
+fgp= folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'orange' if x['properties']['POP2005'] < 10000000
+else 'red' if 10000000<=x['properties']['POP2005']< 20000000 else 'purple'}))
+map.add_child(fgp)
+fgv= folium.FeatureGroup(name="Volcano")
 for lt, ln, vnm, ev  in zip(lat, lon,nm, elev):
     #txt= "Name: "+vnm+"<br>Elevation: "+str(ev) + " m"
     #popup = folium.Popup(txt, max_width=200, min_width = 100)
-    iframe = folium.IFrame(html=html % (vnm, vnm, ev), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_prod(ev))))
-map.add_child(fg)
-map.save("Map1.html")
+    iframe = folium.IFrame(html=html % (vnm, vnm, ev), width=200, height=75)
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], popup=folium.Popup(iframe,max_width=300, min_width = 100), radius=6, fill=True, fill_color=color_prod(ev), color='grey', fill_opacity=0.75))
+
+
+map.add_child(fgv)
+map.add_child(folium.LayerControl())
+map.save("index.html")
